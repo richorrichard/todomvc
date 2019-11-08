b0e7e4436793ee143ff428b08407801a37156bce
Richard Sherman <hello@richardsherman.com>
2019-11-08 12:30:35 -0700

    make App.render method honest be removing storage functionality

--- examples/jquery/js/app.js
+++ examples/jquery/js/app.js
@@ -49,6 +49,7 @@ jQuery(function ($) {
 				'/:filter': function (filter) {
 					this.filter = filter;
 					this.render();
+					util.store('todos-jquery', this.todos);
 				}.bind(this)
 			}).init('/all');
 		},
@@ -70,7 +71,6 @@ jQuery(function ($) {
 			$('#toggle-all').prop('checked', this.getActiveTodos().length === 0);
 			this.renderFooter();
 			$('#new-todo').focus();
-			util.store('todos-jquery', this.todos);
 		},
 		renderFooter: function () {
 			var todoCount = this.todos.length;
@@ -92,6 +92,7 @@ jQuery(function ($) {
 			});
 
 			this.render();
+			util.store('todos-jquery', this.todos);
 		},
 		getActiveTodos: function () {
 			return this.todos.filter(function (todo) {
@@ -118,6 +119,7 @@ jQuery(function ($) {
 			this.todos = this.getActiveTodos();
 			this.filter = 'all';
 			this.render();
+			util.store('todos-jquery', this.todos);
 		},
 		// accepts an element from inside the `.item` div and
 		// returns the corresponding index in the `todos` array
@@ -149,11 +151,13 @@ jQuery(function ($) {
 			$input.val('');
 
 			this.render();
+			util.store('todos-jquery', this.todos);
 		},
 		toggle: function (e) {
 			var i = this.indexFromEl(e.target);
 			this.todos[i].completed = !this.todos[i].completed;
 			this.render();
+			util.store('todos-jquery', this.todos);
 		},
 		edit: function (e) {
 			var $input = $(e.target).closest('li').addClass('editing').find('.edit');
@@ -185,10 +189,12 @@ jQuery(function ($) {
 			}
 
 			this.render();
+			util.store('todos-jquery', this.todos);
 		},
 		destroy: function (e) {
 			this.todos.splice(this.indexFromEl(e.target), 1);
 			this.render();
+			util.store('todos-jquery', this.todos);
 		}
 	};
 
